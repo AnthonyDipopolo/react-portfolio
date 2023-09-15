1abbf1eae446d82c466307bdc61b114061996df0
AnthonyDipopolo <anthony.dipopolo@gmail.com>
2023-09-15 18:42:30 -0400

    added google analytics

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,19 @@
 import HomePage from './pages/HomePage';
+import ReactGA from 'react-ga';
+
+// Uuse 'createBrowserHistory' from 'history' package to enable page tracking
+import { createBrowserHistory } from 'history';
+
+const measurementId = 'G-HD8ZP6XW7W';
+
+// Initialize Google Analytics with Measurement ID
+ReactGA.initialize(measurementId);
+
+// Create a history object for tracking page views
+const history = createBrowserHistory();
+
+// Initialize ReactGA and set it to use history object
+ReactGA.initialize(measurementId, { history });
 
 function App() {
   return (
